a4ab4baa925edf36711fdf005d512ad094b2fb19
sudi <kekovasudi@gmail.com>
2020-08-29 15:52:04 +0300

    adding storage packages and configurations

---
+++ database/migrations/2020_08_29_085843_add_file_url_column_in_calls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFileUrlColumnInCallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('calls', function (Blueprint $table) {
+            //
+            DB::update('alter table `calls` add column file_url text null after call_link');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('calls', function (Blueprint $table) {
+            //
+        });
+    }
+}
